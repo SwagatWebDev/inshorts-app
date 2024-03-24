ef440ee275d75acbc89c072e7dad5b6f7df5fb68
swagat030 <swagatkumarmishra888@gmail.com>
2024-03-25 01:44:14 +0530

    fixed card height

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,6 @@ export const LOGO_URL = "https://www.mediainfoline.com/wp-content/uploads/2015/1
 
 export const NEWS_DATA = [
     {
-        imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTr-F0EgNyAOYhGaIuianHdfmI5B8BzAOBCJO-ik7bqEyuyjoyL",
-        title: "Arvind Kejriwal appears before Delhi court over ED summons in liquor policy case, granted bail",
-        details: "Delhi CM Arvind Kejriwal has been granted bail by the Rouse Avenue Court in connection with the Delhi Excise Policy case. Kejriwal appeared before the court on Saturday after skipping eight calls by the Enforcement Directorate in this connection. He appeared before ACMM Divya Malhotra following summons issued to him by the court on the basis of two ED complaints.",
-        link: "https://news.abplive.com/delhi-ncr/delhi-cm-arvind-kejriwal-gets-bail-in-delhi-excise-policy-case-court-asked-to-furnish-bond-for-rs-15-000-1672377/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
-        shortDetails: 'Arvind Kejriwal on Delhi court over ED summons'
-    },
-    {
         imageUrl: "https://www.hindustantimes.com/ht-img/img/2023/06/10/550x309/Yasin-Malik--who-is-serving-life-imprisonment-in-a_1677259753641_1686391214904.jpg",
         title: "Govt bans Yasin Malik's Jammu Kashmir Liberation Front (JKLF) as for another 5 years",
         details: "The Centre on Saturday extended the ban on Yasin Malik's Jammu Kashmir Liberation Front (JKLF) as an \"unlawful association\" by five years. \"The banned outfit continues to engage in activities that foment terror and secessionism in J&K,\" Union Home Minister Amit Shah said. \"The Modi government will remain unsparing to people and organizations involved in terror activities,\" he further said.",
@@ -21,5 +14,75 @@ export const NEWS_DATA = [
         details: "Union Home Minister Amit Shah, reacting to the Supreme Court's verdict on electoral bonds, said, \"I fully respect the...decision. But I feel...instead of completely scrapping the electoral bonds, it should've been improved.\" \"Out of the total ₹20,000 crore electoral bonds, the BJP has got approximately ₹6,000 crore. Where did the rest of the bonds go?\" he added.",
         link: "https://www.hindustantimes.com/india-news/amit-shah-reacts-to-electoral-bonds-verdict-fully-respect-order-but-101710547406807-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
         shortDetails: 'BJP got ₹6,000 crore, rest go?'
+    },
+    {
+        imageUrl: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTr-F0EgNyAOYhGaIuianHdfmI5B8BzAOBCJO-ik7bqEyuyjoyL",
+        title: "Arvind Kejriwal appears before Delhi court over ED summons in liquor policy case, granted bail",
+        details: "Delhi CM Arvind Kejriwal has been granted bail by the Rouse Avenue Court in connection with the Delhi Excise Policy case. Kejriwal appeared before the court on Saturday after skipping eight calls by the Enforcement Directorate in this connection. He appeared before ACMM Divya Malhotra following summons issued to him by the court on the basis of two ED complaints.",
+        link: "https://news.abplive.com/delhi-ncr/delhi-cm-arvind-kejriwal-gets-bail-in-delhi-excise-policy-case-court-asked-to-furnish-bond-for-rs-15-000-1672377/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: 'Arvind Kejriwal on Delhi court over ED summons'
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711280851891_532.jpg?",
+        title: "1 held as group forcibly puts Holi colours on Muslim family in UP",
+        details: "One person has been arrested and three minors were detained after a video showing a group forcibly applying Holi colours on a Muslim family in Uttar Pradesh's Bijnor went viral. The family, which included two women, were travelling on a bike and were on their way to get medicines when the group intercepted them, police said.",
+        link: "https://www.siasat.com/up-muslim-family-harassed-by-group-playing-holi-1-arrested-2997680/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: 'held as group forcibly puts Holi colours on Muslim family'
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/webp/xs/2024/03_mar/24_sun/img_1711300280894_576.webp?&quot;);",
+        title: "BJP fields K Surendran against Rahul Gandhi in Wayanad",
+        details: "BJP has released its fifth list of 111 Lok Sabha candidates and fielded the party's Kerala unit chief K Surendran against Congress leader Rahul Gandhi from Wayanad. Rahul won from the seat in the Lok Sabha elections in 2019. BJP has also fielded actress Kangana Ranaut from Himachal Pradesh's Mandi.",
+        link: "https://twitter.com/ANI/status/1771924807933685925?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: 'BJP fields K Surendran in Wayanad'
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711281840293_520.jpg?",
+        title: "Missile fired by Russia enters Poland's airspace",
+        details: "A Russian cruise missile fired at western Ukrainian towns breached Poland's airspace, Polish army said. Polish airspace was breached by one of the cruise missiles fired in the night by the air forces...of Russian Federation The object flew through Polish airspace above Oserdow village and stayed for 39 seconds",
+        link: "https://www.news18.com/amp/world/russian-cruise-missile-breaches-polish-airspace-army-8827024.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: 'Missile fired by Russia'
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711281752285_926.jpg?",
+        title: "Well aware of every street of Kashi: Rai on being fielded against PM",
+        details: "Congress' Ajay Rai, who has been fielded by the party from Varanasi against PM Narendra Modi, said, \"I am from Kashi and I am well aware of every street, house and person in Kashi.\" He added, \"The time will tell whether BJP will win or lose from the margin of 10 lakh.\" Lok Sabha elections to begin on April 19.",
+        link: "https://twitter.com/ANI/status/1771863557115658671?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: "Congress released 4th list of LS candidates"
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711279602052_207.jpg?",
+        title: "Shah Rukh Khan meets Rinku Singh's family after KKR's win, Rinku shares pic",
+        details: "Kolkata Knight Riders (KKR) co-owner Shah Rukh Khan met batter Rinku Singh's family after their win against SunRisers Hyderabad (SRH) in their IPL 2024 opener at Eden Gardens in Kolkata. Rinku shared a picture on Instagram, writing, \"The ones who make my heart smile.\" KKR defeated SRH by four runs.",
+        link: "https://www.hindustantimes.com/entertainment/bollywood/ipl-2024-shah-rukh-khan-poses-for-a-click-with-cricketer-rinku-singh-and-family-101711272853764-amp.html?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: "KKR will next face RCB on Friday"
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711281052252_348.jpg?",
+        title: "Kejriwal in jail but still clinging on to CM's post: INC's Sanjay",
+        details: "Congress leader Sanjay Nirupam, while referring to Delhi CM Arvind Kejriwal's arrest in the liquor policy case, said, \"I have sympathy for him as a human being.\" \"The court has to decide what is the truth of Delhi's liquor scam. He's in custody but is still clinging to the post of Chief Minister?\" Nirupam said.",
+        link: "https://twitter.com/sanjaynirupam/status/1771411038564503682?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1771411038564503682%7Ctwgr%5Ef58be73f15b8702a70ffb6490ec5e3804402ff46%7Ctwcon%5Es1_&utm_campaign=fullarticle&utm_medium=referral&ref_url=https%3A%2F%2Fwww.ndtv.com%2Findia-news%2Fhurting-indian-politics-congress-leader-sanjay-nirupam-on-arvind-kejriwal-continuing-as-chief-minister-5296283&utm_source=inshorts",
+        shortDetails: "'He should immediately resign from his post'"
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711279187403_175.jpg?",
+        title: "Video shows foreigner woman's obscene dance with men at Holi party in UP, police take action",
+        details: "A video showing an obscene dance between several men and a foreigner woman at a Holi party in Uttar Pradesh's Mathura has surfaced online. The party was allegedly attended by famous builders of Mathura. Responding to videos posted online, Mathura Police said that the party was organised without obtaining necessary permission and a case has been registered in this matter.",
+        link: "https://www.freepressjournal.in/amp/viral/unholy-holi-in-mathura-vulgar-celebrations-by-builders-lead-to-public-outcry-see-viral-video?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        shortDetails: "To know more"
+    },
+    {
+        title: "Maldives President does a U-turn, calls India 'closest ally'",
+        details: "Maldives President Muizzu said in an interview that India remains their 'closest ally'. This remark comes when they're seeking debt relief from India. At the end of 2023, Maldives already owed US$400.9 to India. Notably, one of the cornerstones of Muizzu's presidential campaign was the 'India out' strategy. They've followed through on it, warming up to China, until this comment.",
+        link: "https://www.business-standard.com/external-affairs-defence-security/news/maldives-president-muizzu-calls-india-it-s-closest-ally-seeks-debt-relief-124032200894_1.html?utm_source=inshorts&utm_medium=referral&utm_campaign=fullarticle",
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711280898437_588.jpg?",
+        shortDetails: "Maldives President does a U-turn, calls India 'closest ally'",
+    },
+    {
+        imageUrl: "https://nis-gs.pix.in/inshorts/images/v1/variants/jpg/m/2024/03_mar/24_sun/img_1711282923889_910.jpg?",
+        title: "Accenture quarterly results shocked the markets!",
+        details: "Not considering COVID quarters, Accenture had NEVER lowered their revenue guidance in the last 10 years. However, in the results released last Thursday they lowered their revenue guidance by 2 percentage. And the most interesting aspect is that the expected growth is also coming through acquisitions and not organic growth. What do you takeaway as corporate employees though?",
+        link: "https://youtu.be/M280vBDmlC0?utm_source=inshorts&utm_medium=referral&utm_campaign=fullarticle",
+        shortDetails: 'Accenture quarterly results shocked the markets!'
     }
 ];
